3e4d7335ea7be44129234b85861f44011f5ed89f
MarcelFeo <marcelfeo29@gmail.com>
2023-06-27 15:57:56 -0300

    Adiciona faker no seed e atualiza o Rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Hero.delete_all
+
+# cria 5 heróis no banco de dados usando a gem 'Faker' para gerar nomes aleatórios de heróis
+5.times do
+  Hero.create name: Faker::Superhero.name
+end
