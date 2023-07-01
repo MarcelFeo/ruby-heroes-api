5df89bb730fb45bfbb99d7693de5673e92c0898f
MarcelFeo <marcelfeo29@gmail.com>
2023-07-01 17:24:45 -0300

    Consertando um erro de sintaxe

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace: :api, defaults: { format: :json } do
+  namespace :api, defaults: { format: :json } do
     resources :heroes
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
